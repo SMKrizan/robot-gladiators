aa78b8e6c30044bf2bfc57a1945c9e7e0db9842b
smkrizan <smkrizan@gmail.com>
2020-09-14 13:25:38 -0500

    first lines of code w notes

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert! Javascript is running!");
+//this creates a function named "fight" using an object called "window" associated with a particular set of internal actions and tools; objects are accessed via connection to functions (in this case "prompt") using dot notation. Function+object="method". The "var" keyword and variable name "playerName" (camel casing for more than one word is best practice - JS cannot interpret hyphens) will store the value passed by user with aid of assignment operator "=" and statement terminator ";". //
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+window.alert(playerName);
+function fight() {
+    // this is an expression containing a function with "function" keyword and function name "fight"; it is a best practice to name functions based on their functionality. The code wrapped in curly braces is the "code block"
+    window.alert("The fight has begun!");
+}
+// fight();
