506fe5a6ca992c382a0c6f4f6f89d347e9cc9536
smkrizan <smkrizan@gmail.com>
2020-09-15 12:23:14 -0500

    SKIP FIGHT if else statements added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,104 @@
-//this creates a function named "fight" using an object called "window" associated with a particular set of internal actions and tools; objects are accessed via connection to functions (in this case "prompt") using dot notation. Function+object="method". The "var" keyword and variable name "playerName" (camel casing for more than one word is best practice - JS cannot interpret hyphens) will store the value passed by user with aid of assignment operator "=" and statement terminator ";". //
-var playerName = window.prompt("What is your robot's name?");
-// Note the lack of quotation marks around playerName
-// the following method "developer console" is used to communicate info to developers (rather than using a window prompt which is generally for communicating to users). Like "window", "console" is an object provided by the browswer - an API - outside of the JS language but which interfaces with the browser console window.
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-//this will do math and log the sum
-console.log(10 + 10);
-//what is this?
+var playerName = window.prompt("What is you robot's name?");
+//note the lack of quotation marks around playerName
 console.log("Our robot's name is " + playerName);
-function fight() {
-    // this is an expression containing a function with "function" keyword and function name "fight"; it is a best practice to name functions based on their functionality. The code wrapped in curly braces is the "code block"
-    window.alert("The fight has begun!");
-}
-// fight();
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+// Before building even a simple function is is best practice to outline the steps describing what the function will accomplish as preparation, as follows:
+var fight = function () {
+    // ask users whether they want to fight or skip the battle
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    console.log(promptFight);
+
+    //if player chooses to fight, then fight
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+        //remove enemy's health by subtracting the amount set in the playerAttack variable
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(
+            playerName + " attacked " + enemyName + ". " + enemyName + "now has " + enemyHealth + " health remaining.");
+
+        //check enemy's health
+        if (enemyHealth <= 0) {
+            window.alert(enemyName + " has died!");
+        } else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        }
+
+        //remove player's health by subtracting the amount set in the enemyAttack variable
+        playerHealth = playerHealth - enemyAttack;
+        console.log(
+            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+        //check player's health
+        if (playerHealth <= 0) {
+            window.alert(playerName + " has died!");
+        } else {
+            window.alert(playerName + " still has " + playerHealth + "health left.")
+        }
+        //if player chooses to skip
+        } else if (promptFight === "skip" || promptFight === "SKIP") {
+            //confirm user wants to skip
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+            //if yes (true), leave fight
+            if (confirmSkip) {
+                window.alert(playerName + " has decided to skip this fight. Goodbye!");
+                //subtract money from PlayerMoney for skipping
+                playerMoney = playerMoney - 2;
+            }
+            // if no (false), ask question again by running fight() again
+            else {
+                fight();
+            }
+        }
+    }
+    //alert users that round is starting
+    window.alert("Welcome to Robot Gladiators!");
+    window.alert(playerName + " is ready for battle!");
+
+    //subtract value of 'playerAttack' from value of 'enemyHealth' and update 'enemyHealth' variable (this is called 'scoping' a variable).
+    enemyHealth = enemyHealth - playerAttack;
+
+    //log result message to console to confirm
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    )
+    //subtract value of enemyAttack from playerHealth and update 'playerHealth' variable
+    playerHealth = playerHealth - enemyAttack;
+
+    //log result message to console as confirmation
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+    
+    //check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!")
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+    
+    //check to see if the value of the playerHealth variable is greater than 0
+    if (playerHealth > 0) {
+        console.log("Your player is still alive!");
+    }
+
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+// execute function
+fight();
